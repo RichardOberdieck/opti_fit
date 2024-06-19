c094625fcd9f2cd3d96eee22188c7eaa1c7800e1
Richard Oberdieck <rico@bankingcircle.com>
2024-06-19 10:09:38 +0200

    First working model

--- opti_fit/dataset_utils.py
+++ opti_fit/dataset_utils.py
@@ -3,12 +3,12 @@ from enum import Enum
 
 
 class Algorithm(str, Enum):
-    REGEX_MATCH = " Regex Match"
-    JARO_WINKLER = " Jaro Winkler"
-    FUZZ_RATIO = " Fuzz Ratio"
-    FUZZ_PARTIAL_RATIO = " Fuzz Partial Ratio"
-    FUZZ_TOKEN_SORT_RATIO = " Fuzz Token Sort Ratio"
-    FUZZ_PARTIAL_TOKEN_SORT_RATIO = " Fuzz Partial Token Sort Ratio"
+    REGEX_MATCH = "regex_match"
+    JARO_WINKLER = "jaro_winkler"
+    FUZZ_RATIO = "fuzz_ratio"
+    FUZZ_PARTIAL_RATIO = "fuzz_partial_ratio"
+    FUZZ_TOKEN_SORT_RATIO = "fuzz_token_sort_ratio"
+    FUZZ_PARTIAL_TOKEN_SORT_RATIO = "fuzz_partial_token_sort_ratio"
 
 
 ALGORITHMS = [a for a in set(Algorithm)]  # Want a list of easy pandas code
@@ -32,17 +32,15 @@ def read_dataset(filename: str) -> pd.DataFrame:
 def _validate_dataset(df: pd.DataFrame) -> None:
     # Validation 1 - column names
     assert set(df.columns) == {
-        "payment_case_id",
-        " Fuzz Partial Token Sort Ratio",
-        " Fuzz Token Sort Ratio",
-        " Fuzz Ratio",
-        " Fuzz Partial Ratio",
-        " Jaro Winkler",
-        " is_payment_true_hit",
-        " is_hit_true_hit",
-        " Regex Match",
-    }
+        "payment_id",
+        "is_payment_true_hit",
+        "is_hit_true_hit",
+    }.union(ALGORITHMS)
 
     # Validation 2 - all scores are between 0 and 100
     assert df[ALGORITHMS].min(axis=None) >= 0
     assert df[ALGORITHMS].max(axis=None) <= 100
+
+
+def remove_payment_hits_without_true_hits(df: pd.DataFrame) -> pd.DataFrame:
+    pass
--- opti_fit/simple_model.py
+++ opti_fit/simple_model.py
@@ -1,47 +1,47 @@
 import pandas as pd
 from mip import Model, CBC, CONTINUOUS, BINARY, xsum, minimize
 
-from opti_fit.dataset_utils import ALGORITHMS
+from opti_fit.dataset_utils import ALGORITHMS, CUTOFF_THRESHOLDS
 
 
-def solve_simple_model_using_mip(df: pd.DataFrame):
-    # Extract data - may be moved out if too elaborate
-    lb_for_cutoff = df[ALGORITHMS].min(axis=0)
+def solve_simple_model_using_mip(df: pd.DataFrame, mps_filename: str | None = None):
     n_names = len(df)
 
     model = Model(solver_name=CBC)
 
     # Add the variables
     x = {
-        a: model.add_var(f"x_{a}", var_type=CONTINUOUS, lb=lb_for_cutoff[a], ub=100)
+        a: model.add_var(f"x_{a}", var_type=CONTINUOUS, lb=CUTOFF_THRESHOLDS[a], ub=100)
         for a in ALGORITHMS
     }
     y = {
         (a, n): model.add_var(f"y_{a},{n}", var_type=BINARY)
         for a in ALGORITHMS
         for n in range(n_names)
+        if df.loc[n, a] >= CUTOFF_THRESHOLDS[a]
     }
     z = {n: model.add_var(f"z_{n}", var_type=BINARY) for n in range(n_names)}
 
     # Add constraints
-    objective = 0
-    for h, row in df.iterrows():
-        model += z[h] <= xsum(y[a, h] for a in ALGORITHMS if (a, h) in y)
+    objective = []
+    for index, row in df.iterrows():
+        model += z[index] <= xsum(y[a, index] for a in ALGORITHMS if (a, index) in y)
 
-        if row[" is_hit_true_hit"]:
-            model += z[h] == 1
+        if row["is_hit_true_hit"]:
+            model += z[index] == 1
         else:
-            objective.append(z[h])
+            objective.append(z[index])
 
         for a in ALGORITHMS:
-            if (a, h) in y:
-                model += (row[a] - 100) * y[a, h] >= x[a] - 100
-                model += row[a] - row[a] * y[a, h] <= x[a] - 0.01
-                model += y[a, h] <= z[h]
+            if (a, index) in y:
+                model += (row[a] - 100) * y[a, index] >= x[a] - 100
+                model += row[a] - row[a] * y[a, index] <= x[a] - 0.01
+                model += y[a, index] <= z[index]
 
     # Add objective
     model.objective = minimize(xsum(objective))
-    model.write("model.mps")
+    if mps_filename is not None:
+        model.write(mps_filename)
     model.optimize(max_seconds=600)
 
     cut_offs = {a: v.x for a, v in x.items()}
