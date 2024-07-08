b67029c0f020462ed460af26dd6891d0585c223d
Richard Oberdieck <r.oberdieck@gmail.com>
2024-07-08 19:47:30 +0000

    Fix comparison and add script

---
+++ runs.sh
@@ -0,0 +1,8 @@
+hatch run simple --model_type='hit' --solver_name='GUROBI'
+hatch run simple --model_type='payment' --solver_name='GUROBI'
+hatch run simple --model_type='combined' --solver_name='GUROBI'
+hatch run relaxed --solver_name='GUROBI' --model_type='hit' --slacks="0.95, 0.96, 0.97, 0.98, 0.99, 0.995, 0.999"
+hatch run combination --solver_name='GUROBI' --base_model_type='hit'
+hatch run combination --solver_name='GUROBI' --base_model_type='combined'
+hatch run combination --solver_name='GUROBI' --base_model_type='payment'
+hatch run relaxed --solver_name='GUROBI' --model_type='payment' --slacks="0.95, 0.96, 0.97, 0.98, 0.99, 0.995, 0.999"
